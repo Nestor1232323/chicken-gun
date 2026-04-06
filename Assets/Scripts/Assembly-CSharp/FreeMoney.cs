e788feb25c7b2387cbc2b1da9e85bcd021c802a0
Nestor1232323 <nestorgamesluda@gmail.com>
2026-04-06 10:19:47 +0300

    Update chicken-gun files

---
+++ Assets/Scripts/Assembly-CSharp/FreeMoney.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeMoney : MonoBehaviour
+{
+    public void Get(int mod)
+    {
+        LocalStore.GiveMoney(mod);
+    }
+}
